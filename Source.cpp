6c4bc6bba60a97067b31313163f904ab752f9c8f
George Cujbă <george.cujba15@gmail.com>
2021-06-27 17:53:39 +0300

    3 Display Player Layout and Implement player move functionality

--- Source.cpp
+++ Source.cpp
@@ -29,6 +29,14 @@ int inputMenuChoice();
 // Dysplay the game table (the 2D matrix)
 void displayTable(char**, int&, int&, char);
 
+// Start the Game
+void startGame(char**&, int&, int&, int&);
+
+// Display the players' layout
+void displayPlayersLayout(int&, char&, int&, int&);
+
+// Get the coordinates of the player's movement
+void makeAMove(int&, int&);
 
 // Main Program
 int main(void) {
@@ -39,6 +47,14 @@ int main(void) {
 	int columnCoord = 0;
 	int menuChoice = 0;
 	char move = ' ';
+
+	/*
+		-> 1 - it's player1's turn
+		-> 2 -> it's player2's turn
+		-> 0 -> the game ended
+	*/
+	int gameStatus = 1;
+
 
 	allocateMemoryForTable(table);
 	createTable(table);
@@ -56,8 +72,11 @@ int main(void) {
 		}
 	} while (menuChoice != 1 && menuChoice != 2);
 
-	deAllocateMemoryForTable(table);
+	while (gameStatus != 0) {
+		startGame(table, lineCoord, columnCoord, gameStatus);
+	}
 
+	deAllocateMemoryForTable(table);
 	return 0;
 }
 
@@ -115,4 +134,52 @@ void displayTable(char** table, int& line, int& column, char move) {
 	}
 	printf(" - - -\n");
 	printf(LINE_SEPARATOR);
+}
+
+void startGame(char**& table, int& lineCoord, int& columnCoord, int& gameStatus) {
+	char move;
+	displayPlayersLayout(gameStatus, move, lineCoord, columnCoord);
+	displayTable(table, lineCoord, columnCoord, move);
+}
+
+void displayPlayersLayout(int& gameStatus, char& move, int& line, int& column) {
+
+	if (gameStatus == 1) {
+		move = 'X';
+		printf("\n X Make a move!\n");
+		makeAMove(line, column);
+		gameStatus == 1 ? gameStatus = 2 : gameStatus = 1;
+	}
+	else if (gameStatus == 2) {
+		move = '0';
+		printf("\n 0 Make a move!\n");
+		makeAMove(line, column);
+		gameStatus == 2 ? gameStatus = 1 : gameStatus = 2;
+	}
+
+}
+
+void makeAMove(int& line, int& column) {
+
+	printf("\nEnter the line of your move: ");
+
+	do {
+		if (column != 0 && column != 1 && column != 2) {
+			printf("Please try another value! (column)");
+			printf(LINE_SEPARATOR);
+		}
+
+		scanf_s("%d", &line);
+	} while (line != 0 && line != 1 && line != 2);
+
+	printf("Enter the column of your move: ");
+	do {
+
+		if (column != 0 && column != 1 && column != 2) {
+			printf("Please try another value! (column)");
+			printf(LINE_SEPARATOR);
+		}
+
+		scanf_s("%d", &column);
+	} while (column != 0 && column != 1 && column != 2);
 }
