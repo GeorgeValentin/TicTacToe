917537415008a0cc2911a610d2a8a84c22697b96
George Cujbă <george.cujba15@gmail.com>
2021-06-27 18:59:10 +0300

    4 Validate Player Input to Accept only Three Values

--- Source.cpp
+++ Source.cpp
@@ -1,15 +1,26 @@
 #define _CRT_SECURE_NO_WARNINGS
 
+/*
+TO IMPLEMENT:
+-> Validate input so that a player cannot move in an occupied table cell
+-> Implement game end - win, draw, lose
+*/
+
 // Macros variables
 #define NUMBER_OF_LINES 3
 #define NUMBER_OF_COLUMNS 3
+#define COUNT_ONE_DIGIT_NUMBERS 3
 #define LINE_SEPARATOR "\n"
 
 // Libraries used
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-// Function prototypes
+// Global variables
+char numChars[] = { '0', '1', '2' };
+
+/* Function prototypes */ 
 
 // Allocate memory for the game table (2x2 char matrix)
 void allocateMemoryForTable(char**&);
@@ -37,6 +48,8 @@ void displayPlayersLayout(int&, char&, int&, int&);
 
 // Get the coordinates of the player's movement
 void makeAMove(int&, int&);
+
+void validateCoordInputMove(char&, bool&, int&);
 
 // Main Program
 int main(void) {
@@ -139,6 +152,22 @@ void displayTable(char** table, int& line, int& column, char move) {
 void startGame(char**& table, int& lineCoord, int& columnCoord, int& gameStatus) {
 	char move;
 	displayPlayersLayout(gameStatus, move, lineCoord, columnCoord);
+
+	for (int i = 0; i < NUMBER_OF_LINES; i++) {
+		for (int j = 0; j < NUMBER_OF_COLUMNS; j++) {
+
+			// ****** fix this bug
+			//if (move == table[i][j]) {
+			//	printf("Cell occupied. Please make another move!");
+			//	makeAMove(lineCoord, columnCoord);
+			//}
+
+			//if (table[i][j] == ) {
+			//	gameStatus = 0;
+			//}
+		}
+	}
+
 	displayTable(table, lineCoord, columnCoord, move);
 }
 
@@ -148,38 +177,69 @@ void displayPlayersLayout(int& gameStatus, char& move, int& line, int& column) {
 		move = 'X';
 		printf("\n X Make a move!\n");
 		makeAMove(line, column);
-		gameStatus == 1 ? gameStatus = 2 : gameStatus = 1;
+		gameStatus == 1 ? gameStatus = 2 : gameStatus = 0;
 	}
 	else if (gameStatus == 2) {
 		move = '0';
 		printf("\n 0 Make a move!\n");
 		makeAMove(line, column);
-		gameStatus == 2 ? gameStatus = 1 : gameStatus = 2;
+		gameStatus == 2 ? gameStatus = 1 : gameStatus = 0;
 	}
+}
 
+void validateCoordInputMove(char &coordUserInputMove, bool &isCoordInputADigit, int &coord) {
+	for (int i = 0; i < COUNT_ONE_DIGIT_NUMBERS; i++) {		
+		if (coordUserInputMove != numChars[i]) {
+			isCoordInputADigit = false;
+			coord = -1;
+		}
+		else {
+			isCoordInputADigit = true;
+			break;
+		}
+	}
 }
 
 void makeAMove(int& line, int& column) {
 
+	bool isLineInputADigit = true;
+	bool isColumnInputADigit = true;
+	
+	char lineUserInput = ' ';
+	char columnUserInput = ' ';
+
 	printf("\nEnter the line of your move: ");
+	scanf(" %c", &lineUserInput);
 
 	do {
-		if (column != 0 && column != 1 && column != 2) {
-			printf("Please try another value! (column)");
-			printf(LINE_SEPARATOR);
+		validateCoordInputMove(lineUserInput, isLineInputADigit, line);
+
+		if (isLineInputADigit) {
+			// convert char to int
+			line = lineUserInput - '0';
+		}
+		else {
+			printf("Please enter another value: ");
+			scanf(" %c", &lineUserInput);
 		}
 
-		scanf_s("%d", &line);
-	} while (line != 0 && line != 1 && line != 2);
+
+	} while (line != 0 && line != 1 && line != 2 && isLineInputADigit == false);
 
 	printf("Enter the column of your move: ");
+	scanf(" %c", &columnUserInput);
+
 	do {
+		validateCoordInputMove(columnUserInput, isColumnInputADigit, column);
 
-		if (column != 0 && column != 1 && column != 2) {
-			printf("Please try another value! (column)");
-			printf(LINE_SEPARATOR);
+		if (isColumnInputADigit) {
+			// convert char to int
+			column = columnUserInput - '0';
 		}
-
-		scanf_s("%d", &column);
-	} while (column != 0 && column != 1 && column != 2);
+		else {
+			printf("Please enter another value: ");
+			scanf(" %c", &columnUserInput);
+		}
+	} while (column != 0 && column != 1 && column != 2 && isColumnInputADigit == false);
 }
+
